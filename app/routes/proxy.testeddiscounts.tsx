9307f3efb69b202dab3b62946fbb83f0b932408d
khalfa <khalfaradhouene@gmail.com>
2026-02-06 13:40:45 +0100

    FEAT:NEW COMMIT

--- app/routes/proxy.testeddiscounts.tsx
+++ app/routes/proxy.testeddiscounts.tsx
@@ -85,7 +85,7 @@ const variants =
   json?.data?.productVariants?.edges ?? [];
   console.log("Shopify variants is her hello:", variants);
 const continueVariants = variants
-.filter(({ node }: any) => node.inventoryPolicy === "DENY")
+.filter(({ node }: any) => node.inventoryPolicy === "CONTINUE")
 // .map(({ node }:any) => ({
 //   id: node.id,
 //   inventoryPolicy: "CONTINUE"
@@ -123,7 +123,7 @@ if (continueVariants.length > 0) {
             productId: node.product.id,
             variants: {
                id: node.id,
-              inventoryPolicy: "CONTINUE"
+              inventoryPolicy: "DENY"
             }
           }
         }
