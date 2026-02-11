546da2c50386060a6100ce6d7484c894043a77be
khalfa <khalfaradhouene@gmail.com>
2026-02-11 16:06:27 +0100

    is new commit

--- app/routes/proxy.descreption.tsx
+++ app/routes/proxy.descreption.tsx
@@ -4,29 +4,36 @@ import { type LoaderFunctionArgs } from "@remix-run/node";
 
 export async function loader({context,request}:LoaderFunctionArgs){
     const response = await fetch("http://localhost:11434/api/generate", {method:"post",
-        model: "deepseek-coder",
-        prompt: `
-    You are an expert eCommerce SEO specialist.
-    
-    Rewrite the following product description into:
-    - Professional HTML
-    - SEO optimized
-    - Use semantic tags
-    - Add headings (H2, H3)
-    - Add bullet benefits
-    - Add call-to-action
-    - Keep Shopify compatible
-    - Return ONLY clean HTML
-    
-    HTML:
-    <div>
-      <p>Good leather wallet for men. Brown color. Handmade.</p>
-    </div>
-    `,
-        stream: false,
-        options: {
-          temperature: 0.7
-        }
+        body:JSON.stringify(
+            {
+                options: {
+                    temperature: 0.7
+                  },
+                stream: false,
+                model: "deepseek-coder",
+                prompt: `
+            You are an expert eCommerce SEO specialist.
+            
+            Rewrite the following product description into:
+            - Professional HTML
+            - SEO optimized
+            - Use semantic tags
+            - Add headings (H2, H3)
+            - Add bullet benefits
+            - Add call-to-action
+            - Keep Shopify compatible
+            - Return ONLY clean HTML
+            
+            HTML:
+            <div>
+              <p>Good leather wallet for men. Brown color. Handmade.</p>
+            </div>
+            `,
+            }
+        ),
+      
+       
+        
       });
 let res=await response.json()
 
