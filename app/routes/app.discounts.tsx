b5033ca971311f7277f79a301e22cf0af7c88a71
khalfa <khalfaradhouene@gmail.com>
2026-02-13 00:20:46 +0100

    wwww

--- app/routes/app.discounts.tsx
+++ app/routes/app.discounts.tsx
@@ -216,7 +216,7 @@ const location=useLocation()
           onClick={() => {
             setCursorStack(prev => [...prev, pageInfo.endCursor]);
             fetcher.load(
-              `${location.pathname}?cursor=${encodeURIComponent(pageInfo.endCursor)}`
+              `?cursor=${encodeURIComponent(pageInfo.endCursor)}`
             );
             
           }}
