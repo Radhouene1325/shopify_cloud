b540687f2221719b8dbded7a06fa7bead8afc6f4
khalfa <khalfaradhouene@gmail.com>
2026-02-18 19:42:45 +0100

    GEE

--- app/routes/app.descreptionupdated.tsx
+++ app/routes/app.descreptionupdated.tsx
@@ -430,7 +430,7 @@ for(const DESC_AI of optimizedHtml){
           "product": {
             "id": OLD_DESC.id,
             "descriptionHtml": DESC_AI.detailedDescription,
-            "tags":["DESC_AI"],
+            "tags":"DESC_AI",
             "metafields": [
               {
                 "namespace": "custom",
