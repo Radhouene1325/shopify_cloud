1b14f305bcb5529b9b7d8e46ad07acf61fd9d80c
khalfa <khalfaradhouene@gmail.com>
2026-03-09 21:51:19 +0100

    xxx

--- server.ts
+++ server.ts
@@ -525,11 +525,11 @@ console.log('ssssssssssssssss',aggregateRating)
           { namespace: "custom", key: "seo_descreption", type: "json", value: JSON.stringify(SEO.seoDescription) },
           { namespace: "seo", key: "schema_org", type: "json", value: JSON.stringify(productSchema) },
         
-          { namespace: "custom", key: "facebookTitle", type: "json", value:SEO?.socialOptimization.facebookTitle },
-          { namespace: "custom", key: "facebookDescription", type: "json", value:SEO?.socialOptimization.facebookDescription },
-          { namespace: "custom", key: "tiktokTitle", type: "json", value:SEO?.socialOptimization.tiktokTitle },
-          { namespace: "custom", key: "pinterestTitle", type: "json", value:SEO?.socialOptimization.pinterestTitle },
-          { namespace: "custom", key: "pinterestDescription", type: "json", value:SEO?.socialOptimization.pinterestDescription },
+          // { namespace: "custom", key: "facebookTitle", type: "json", value:SEO?.socialOptimization.facebookTitle },
+          // { namespace: "custom", key: "facebookDescription", type: "json", value:SEO?.socialOptimization.facebookDescription },
+          // { namespace: "custom", key: "tiktokTitle", type: "json", value:SEO?.socialOptimization.tiktokTitle },
+          // { namespace: "custom", key: "pinterestTitle", type: "json", value:SEO?.socialOptimization.pinterestTitle },
+          // { namespace: "custom", key: "pinterestDescription", type: "json", value:SEO?.socialOptimization.pinterestDescription },
 
         
         ]
