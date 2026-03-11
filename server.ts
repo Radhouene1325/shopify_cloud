76d5fad68b133287e55e7a2e1050eb2c48d324bf
khalfa <khalfaradhouene@gmail.com>
2026-03-11 20:16:58 +0100

    sss

--- server.ts
+++ server.ts
@@ -88,7 +88,7 @@ await Promise.all(
       
       const other_reviews = await rating.json();
       
-      console.log("rrrrrrrrrrrrrrrrrr",other_reviews?.data?.reviews?.media);
+      console.log("rrrrrrrrrrrrrrrrrr",other_reviews?.data?.reviews?.map(((e:any)=>e.media)));
 
       const admin = createShopifyAdmin(
         shop,
