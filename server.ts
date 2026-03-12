a66015eb276b899b7e81f5449a0c7e85935d546e
khalfa <khalfaradhouene@gmail.com>
2026-03-12 17:35:44 +0100

    mmm

--- server.ts
+++ server.ts
@@ -409,7 +409,7 @@ console.log('Collections for product:', collections);
       // console.log('ddndndnd',productSchema)
 
   
-      console.log("sssssproductSchema is secces",SEO.category?.id)
+      console.log("ssssjjjsproductSchema is secces",SEO.category?.id)
       console.log("productSchema is secces",SEO)
 
       
