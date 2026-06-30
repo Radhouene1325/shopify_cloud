13ce84f2aa04763da63039e66cd19d5ec5b9c6c3
khalfa <khalfaradhouene@gmail.com>
2026-06-30 23:36:48 +0200

    feat: increase product fetch limit from 15 to 30 in TikTok shop policy query

--- app/routes/app.tiktokshoppolicy.tsx
+++ app/routes/app.tiktokshoppolicy.tsx
@@ -228,7 +228,7 @@ export const loader = async ({ request, context }: LoaderFunctionArgs) => {
 
   const query = `#graphql
     query GetTikTokPolicyProducts($cursor: String) {
-      products(first: 15, after: $cursor,  sortKey: PUBLISHED_AT, reverse: true) {
+      products(first: 30, after: $cursor,  sortKey: PUBLISHED_AT, reverse: true) {
         edges {
           node {
             id
